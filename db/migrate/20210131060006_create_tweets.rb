31550ec5186438aa86eb1871d9f7b620b1d2ef02
YOHEYYOHEY <yohey33d0v0v@gmail.com>
2021-01-31 16:21:20 +0900

    tweetモデル作成

---
+++ db/migrate/20210131060006_create_tweets.rb
@@ -0,0 +1,9 @@
+class CreateTweets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tweets do |t|
+      t.integer :money
+      t.string :item
+      t.timestamps
+    end
+  end
+end
